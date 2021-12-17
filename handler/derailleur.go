2b9e8e997a20385044dd2335d813a656796f8109
Jonathan MacDonald <j0nnyappleseed000@mac.com>
2021-12-16 23:50:18 -0600

    Update derailleur handlers

--- handler/derailleur.go
+++ handler/derailleur.go
@@ -2,8 +2,6 @@ package handler
 
 import (
 	"context"
-	"io"
-	"time"
 
 	log "go-micro.dev/v4/logger"
 
@@ -12,54 +10,16 @@ import (
 
 type Derailleur struct{}
 
-func (e *Derailleur) Call(ctx context.Context, req *pb.CallRequest, rsp *pb.CallResponse) error {
+func (e *Derailleur) CreateDerailleur(ctx context.Context, req *pb.CreateDerailleurRequest, rsp *pb.CreateDerailleurResponse) error {
 	log.Infof("Received Derailleur.Call request: %v", req)
-	rsp.Msg = "Hello " + req.Name
+
+	rsp.Derailleur = &pb.BikeDerailleur{
+		Id:           "urn:bicycle:derailleur:1",
+		FrameId:      req.FrameId,
+		Manufacturer: req.Manufacturer,
+		Speed:        req.Speed,
+		Model:        req.Model,
+	}
+
 	return nil
-}
-
-func (e *Derailleur) ClientStream(ctx context.Context, stream pb.Derailleur_ClientStreamStream) error {
-	var count int64
-	for {
-		req, err := stream.Recv()
-		if err == io.EOF {
-			log.Infof("Got %v pings total", count)
-			return stream.SendMsg(&pb.ClientStreamResponse{Count: count})
-		}
-		if err != nil {
-			return err
-		}
-		log.Infof("Got ping %v", req.Stroke)
-		count++
-	}
-}
-
-func (e *Derailleur) ServerStream(ctx context.Context, req *pb.ServerStreamRequest, stream pb.Derailleur_ServerStreamStream) error {
-	log.Infof("Received Derailleur.ServerStream request: %v", req)
-	for i := 0; i < int(req.Count); i++ {
-		log.Infof("Sending %d", i)
-		if err := stream.Send(&pb.ServerStreamResponse{
-			Count: int64(i),
-		}); err != nil {
-			return err
-		}
-		time.Sleep(time.Millisecond * 250)
-	}
-	return nil
-}
-
-func (e *Derailleur) BidiStream(ctx context.Context, stream pb.Derailleur_BidiStreamStream) error {
-	for {
-		req, err := stream.Recv()
-		if err == io.EOF {
-			return nil
-		}
-		if err != nil {
-			return err
-		}
-		log.Infof("Got ping %v", req.Stroke)
-		if err := stream.Send(&pb.BidiStreamResponse{Stroke: req.Stroke}); err != nil {
-			return err
-		}
-	}
 }
